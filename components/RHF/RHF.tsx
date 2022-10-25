e427ec6199deaa61a1a13d353aa0a4aa96b7ad71
MalcolmDahling <mac.dahling@gmail.com>
2022-10-25 13:53:50 +0200

    react hook form validation

--- components/RHF/RHF.tsx
+++ components/RHF/RHF.tsx
@@ -17,12 +17,46 @@ type formValues = {
 
 function RHF({addUser}:IFormProps){
 
-    const {register, handleSubmit, formState: {errors}, reset} = useForm<formValues>();
+    const {register, handleSubmit, formState: {errors}, reset} = useForm<formValues>({mode: 'onTouched'});
 
     function onSubmit(data:IUser){
-        reset();
         addUser(data);
+        reset();
     }
+
+    
+    function validatePhone(phone:string){
+
+        const correctChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', '+', '-'];
+        const phoneArr = phone.split('');
+
+        let check = false;
+
+        for(let i = 0; i < phoneArr.length; i++){
+
+            for(let j = 0; j < correctChars.length; j++){
+
+                if(phoneArr[i] === correctChars[j]){
+                    check = true;
+                }
+            }
+
+            if(check === false){
+                return false;
+            }
+            else{
+                check = false;
+            }
+        }
+
+        return true;
+    }
+    
+
+    function validateEmail(email:string){
+    // eslint-disable-next-line no-useless-escape
+        return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    };
 
 
     return(
@@ -31,16 +65,20 @@ function RHF({addUser}:IFormProps){
 
             <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="firstName">First name </label>
-                <input type="text" placeholder="First name" {...register("firstName")}></input> 
+                <input type="text" placeholder="First name" {...register("firstName", {required: true, minLength: 2})}></input> 
+                {errors.firstName && <p className={styles.error}>Please enter your first name.</p>}
 
                 <label htmlFor="lastName">Last name </label>
-                <input type="text" placeholder="First name" {...register("lastName")}></input>
+                <input type="text" placeholder="First name" {...register("lastName", {required: true, minLength: 2})}></input>
+                {errors.lastName && <p className={styles.error}>Please enter your last name.</p>}
 
                 <label htmlFor="phone">Phone </label>
-                <input type="tel" placeholder="Phone" {...register("phone")}></input>
+                <input type="tel" placeholder="Phone" {...register("phone", {validate: validatePhone})}></input>
+                {errors.phone && <p className={styles.error}>Please enter a valid phone number.</p>}
 
                 <label htmlFor="email">Email </label>
-                <input type="email" placeholder="Email" {...register("email")}></input>
+                <input type="email" placeholder="Email" {...register("email", {required: true, validate: validateEmail})}></input>
+                {errors.email && <p className={styles.error}>Please enter a valid email adress.</p>}
             
                 <input type="submit" value="Submit"></input>
             </form>
