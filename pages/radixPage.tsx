3115fd41984c3f1705635213f6be20100e527f97
MalcolmDahling <mac.dahling@gmail.com>
2022-10-31 13:00:10 +0100

    accordion

--- pages/radixPage.tsx
+++ pages/radixPage.tsx
@@ -1,37 +1,48 @@
 import * as Accordion from '@radix-ui/react-accordion';
 import { styled } from '@stitches/react';
+import styles from '../styles/radixPage.module.scss';
 
 function RadixPage(){
-
-    const Acc = styled('span', {
-        border:'2px solid red'
-    });
 
 
     return(
         <>
-            <Accordion.Root type="single" collapsible>
+            <Accordion.Root type="single" collapsible className={styles.container}>
 
-                <Accordion.Item value="item-1">
-                    <Accordion.Header>
-                        <Accordion.Trigger>
-                            <Acc>asddas</Acc>
-                        </Accordion.Trigger>
-                    </Accordion.Header>
-                    <Accordion.Content>
-                        <span>test</span>
+                <Accordion.Item value="item-0" className={styles.item}>
+                    <Accordion.Header className={styles.header}>
+                        <Accordion.Trigger className={styles.trigger}>Item 0</Accordion.Trigger>
+                    </Accordion.Header >
+                    <Accordion.Content className={styles.content}>
+                        <span className={styles.contentSpan}>asdf</span>
                     </Accordion.Content>
                 </Accordion.Item>
 
 
-                <Accordion.Item value="item-2">
-                    <Accordion.Header>
-                        <Accordion.Trigger>
-                            <span>Test</span>
-                        </Accordion.Trigger>
-                    </Accordion.Header>
-                    <Accordion.Content>
-                        <span>test</span>
+                <Accordion.Item value="item-1" className={styles.item}>
+                    <Accordion.Header className={styles.header}>
+                        <Accordion.Trigger className={styles.trigger}>Item 1</Accordion.Trigger>
+                    </Accordion.Header >
+                    <Accordion.Content className={styles.content}>
+                        <span className={styles.contentSpan}>asdf</span>
+                    </Accordion.Content>
+                </Accordion.Item>
+
+                <Accordion.Item value="item-2" className={styles.item}>
+                    <Accordion.Header className={styles.header}>
+                        <Accordion.Trigger className={styles.trigger}>Item 2</Accordion.Trigger>
+                    </Accordion.Header >
+                    <Accordion.Content className={styles.content}>
+                        <span className={styles.contentSpan}>asdf</span>
+                    </Accordion.Content>
+                </Accordion.Item>
+
+                <Accordion.Item value="item-3" className={styles.item}>
+                    <Accordion.Header className={styles.header}>
+                        <Accordion.Trigger className={styles.trigger}>Item 3</Accordion.Trigger>
+                    </Accordion.Header >
+                    <Accordion.Content className={styles.content}>
+                        <span className={styles.contentSpan}>asdf</span>
                     </Accordion.Content>
                 </Accordion.Item>
 
